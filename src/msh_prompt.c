b0266f3caf1a075ba79f867190b977cd0cf62ffb
Wendrul <dethomassin.etienne@gmail.com>
2021-01-27 20:24:04 +0100

    Close shell on Ctrl + D (EOF check)

--- src/msh_prompt.c
+++ src/msh_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:59:06 by wendrul           #+#    #+#             */
-/*   Updated: 2021/01/26 23:42:00 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/01/27 20:23:18 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@ int		shell(t_builtin builtins)
 	static int	cmd_num = 0;
 	char		*line;
 	t_command	cmd;
+	int			gnl_ret;
 
 	//printf("€ ");
 	write(STDOUT_FILENO, ">:v ", 5);
-	if (get_next_line(STDIN_FILENO, &line) == -1)
+	gnl_ret = get_next_line(STDIN_FILENO, &line);
+	if (gnl_ret == -1)
 		error_exit(FAILED_TO_GET_NEXT_LINE);
+	if (gnl_ret == 0)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		exit(0);
+	}
 	cmd_num++;
 	cmd.num = cmd_num;
 	cmd.argc = parse_into_args(line, &cmd.argv);
