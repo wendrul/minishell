9bc74595db38f7b7bcacc1d7e9575f12e1fe1a18
Wendrul <dethomassin.etienne@gmail.com>
2021-02-05 00:24:52 +0100

    Make a pipe dummy maybe

--- src/msh_execute.c
+++ src/msh_execute.c
@@ -6,7 +6,7 @@
 /*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 23:07:18 by ede-thom          #+#    #+#             */
-/*   Updated: 2021/02/02 20:20:23 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/05 00:22:59 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,8 @@ void execute_pipe(char *line, t_command cmd, t_builtin builtins)
 	pid_t	pid;
 	char	*left;
 	char	*right;
+	int		stdout_cpy;
+
 
 	if (!separate(&left, &right, '|', line))
 	{
@@ -73,15 +75,16 @@ void execute_pipe(char *line, t_command cmd, t_builtin builtins)
 	if (pipe(pipefd))
 		error_exit(FAILED_TO_CREATE_PIPE);
 	pid = fork();
+	stdout_cpy = dup(STDOUT_FILENO);
 	if (pid == (pid_t)0)
 	{
 		/* This is the child process.
          Close other end first. */
 		close(pipefd[1]);
 		dup2(pipefd[0], STDIN_FILENO);
-		run_cmd(right, cmd, builtins);
+		execute_pipe(right, cmd, builtins);
+		close(pipefd[0]);
 		exit(0);
-		return ;
 	}
 	else if (pid < (pid_t)0)
 	{
@@ -93,10 +96,12 @@ void execute_pipe(char *line, t_command cmd, t_builtin builtins)
 		/* This is the parent process.
          Close other end first. */
 		close(pipefd[0]);
-		wait(NULL);
 		dup2(pipefd[1], STDOUT_FILENO);
 		run_cmd(left, cmd, builtins);
-		kill(myself);
-		return ;
+		close(pipefd[1]);
+		close(STDOUT_FILENO);
+		wait(NULL);
+		dup2(stdout_cpy, STDOUT_FILENO);
+		close(stdout_cpy);
 	}
 }
