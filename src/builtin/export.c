742fcf77b4b54c14e0fe62e1cc917fa0d2c46a11
etyene <dethomassin.etienne@gmail.com>
2021-02-20 18:26:29 +0100

    Norm export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,53 +6,64 @@
 /*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 19:53:39 by ede-thom          #+#    #+#             */
-/*   Updated: 2021/02/20 18:00:12 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/20 18:25:20 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	errorset(int error)
+static int	is_valid_var_name(char *str)
 {
-	g_msh->err_no = error;
-	return (-1);
+	int j;
+
+	j = -1;
+	while (str[++j])
+	{
+		if (str[j] == '=')
+			break ;
+		if (ft_indexof(str[j], ALLOWED_VAR_CHARS) == -1 || ft_isdigit(str[0]))
+			return (0);
+	}
+	return (1);
+}
+
+static int	set_envvar(char *str)
+{
+	int			pos;
+	t_var_dict	var;
+
+	if (!((pos = ft_indexof('=', str)) == -1))
+	{
+		if (pos == 0)
+			return (0);
+		else
+			dict_strput(str)->is_env = 1;
+	}
+	else if ((var = dict_get(str)))
+		var->is_env = 1;
+	return (1);
 }
 
 int			msh_export(int argc, char **argv)
 {
 	int			i;
-	int			pos;
-	int			j;
 	int			status;
-	t_var_dict	var;
 
 	i = 0;
 	status = 0;
 	while (++i < argc)
 	{
-		j = -1;
-		while (argv[i][++j])
+		if (!is_valid_var_name(argv[i]))
 		{
-			if (argv[i][j] == '=')
-				break ;
-			if (ft_indexof(argv[i][j], ALLOWED_VAR_CHARS) == -1 || ft_isdigit(argv[i][0]))
-			{
-				status = errorset(BAD_VAR_NAME);
-				continue ;
-			}
+			status = -1;
+			g_msh->err_no = BAD_VAR_NAME;
+			continue ;
 		}
-		if (!((pos = ft_indexof('=', argv[i])) == -1))
+		if (!set_envvar(argv[i]))
 		{
-			if (pos == 0)
-			{
-				status = errorset(EMPTY_VAR_NAME);
-				continue ;
-			}
-			else
-				dict_strput(argv[i])->is_env = 1;
+			status = -1;
+			g_msh->err_no = EMPTY_VAR_NAME;
 		}
-		else if ((var = dict_get(argv[1])))
-			var->is_env = 1;
 	}
 	return (status);
 }
