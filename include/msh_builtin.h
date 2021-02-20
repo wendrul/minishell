941fbd27238abaac33b67fe9a82d97dd5e8cda67
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2021-02-20 13:32:33 +0100

    normed builtin.h

--- include/msh_builtin.h
+++ include/msh_builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   msh_builtin.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agoodwin <agoodwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:59:20 by wendrul           #+#    #+#             */
-/*   Updated: 2021/02/19 18:19:18 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/20 13:32:01 by agoodwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # define PWD_BUFF_SIZE 4096
 
-#include "minishell.h"
+# include "minishell.h"
 
 typedef struct	s_builtin
 {
@@ -24,19 +24,19 @@ typedef struct	s_builtin
 	struct s_builtin	*next;
 }			*	t_builtin;
 
-int		name_cmp(char *str1, char *str2);
-void	free_arr(char **ptr);
-void	print_arr(char **s);
+int				name_cmp(char *str1, char *str2);
+void			free_arr(char **ptr);
+void			print_arr(char **s);
 
-int		run_builtin(t_builtin builtin, char *name, t_command cmd, int *status);
-void	add_builtin(t_builtin *list, char *name, int (*method)(int, char**));
+int				run_builtin(t_builtin builtin, char *n, t_command cmd, int *s);
+void			add_builtin(t_builtin *list, char *n, int (*m)(int, char**));
 
-int		msh_exit(int argc, char **argv);
-int		msh_echo(int argc, char **argv);
-int		msh_env();
-int		msh_pwd();
-int		msh_cd(int argc, char** argv);
-int		msh_unset(int argc, char **argv);
-int		msh_export(int argc, char **argv);
+int				msh_exit(int argc, char **argv);
+int				msh_echo(int argc, char **argv);
+int				msh_env();
+int				msh_pwd();
+int				msh_cd(int argc, char **argv);
+int				msh_unset(int argc, char **argv);
+int				msh_export(int argc, char **argv);
 
 #endif
