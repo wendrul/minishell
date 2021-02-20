a23630d8588d32795a1d94d6b63c522212d218bf
etyene <dethomassin.etienne@gmail.com>
2021-02-20 19:21:23 +0100

    Finishe norme

--- src/msh_parsing2.c
+++ src/msh_parsing2.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   msh_parsing2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agoodwin <agoodwin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 15:32:04 by agoodwin          #+#    #+#             */
-/*   Updated: 2021/02/20 15:34:15 by agoodwin         ###   ########.fr       */
+/*   Updated: 2021/02/20 19:15:11 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_el(void *ptr)
+void		print_el(void *ptr)
 {
 	t_cmd_element e;
 
@@ -20,7 +20,7 @@ void	print_el(void *ptr)
 	printf("(%s, %s), ", e->str, type_name(e->type));
 }
 
-void	add_el(t_list **lst, char *str, int type)
+void		add_el(t_list **lst, char *str, int type)
 {
 	t_list *new;
 
@@ -31,52 +31,57 @@ void	add_el(t_list **lst, char *str, int type)
 	ft_lstadd_back(lst, new);
 }
 
-t_list	*parse_quotes(char *line, t_command cmd)
+static int	quote_loop_logic(char *line, t_command cmd, int i,
+								t_list **elements)
+{
+	char	*str;
+	char	*tmp;
+	int		quote;
+	int		j;
+
+	j = i;
+	quote = line[i];
+	while (line[++j] != line[i])
+		if (!line[j])
+		{
+			shell_error(SYNTAX_ERROR, cmd.num);
+			return (-5);
+		}
+	if (!(tmp = ft_substr(line, i + 1, j - i - 1)))
+		error_exit(MALLOC_FAIL_ERROR);
+	if (quote == '"')
+	{
+		str = place_vars(tmp);
+		add_el(elements, str, TEXT);
+		free(str);
+	}
+	else
+		add_el(elements, tmp, TEXT);
+	free(tmp);
+	return (j);
+}
+
+t_list		*parse_quotes(char *line, t_command cmd)
 {
 	t_list	*elements;
 	char	*tmp;
 	int		i;
-	int		j;
 	int		start;
-	char	quote;
-	char	*str;
 
 	i = -1;
 	start = 0;
 	elements = NULL;
 	while (line[++i])
-	{
 		if (line[i] == '\'' || line[i] == '"')
 		{
-			quote = line[i];
 			if (!(tmp = ft_substr(line, start, i - start)))
 				error_exit(MALLOC_FAIL_ERROR);
 			add_el(&elements, tmp, UNPARSED);
 			free(tmp);
-			j = i;
-			while (line[++j] != line[i])
-			{
-				if (!line[j])
-				{
-					shell_error(SYNTAX_ERROR, cmd.num);
-					return (NULL);
-				}
-			}
-			if (!(tmp = ft_substr(line, i + 1, j - i - 1)))
-				error_exit(MALLOC_FAIL_ERROR);
-			if (quote == '"')
-			{
-				str = place_vars(tmp);
-				add_el(&elements, str, TEXT);
-				free(str);
-			}
-			else
-				add_el(&elements, tmp, TEXT);
-			free(tmp);
-			start = j + 1;
+			if ((start = quote_loop_logic(line, cmd, i, &elements) + 1) < 0)
+				return (NULL);
 			i = start - 1;
 		}
-	}
 	if (!(tmp = ft_substr(line, start, i - start)))
 		error_exit(MALLOC_FAIL_ERROR);
 	add_el(&elements, tmp, UNPARSED);
