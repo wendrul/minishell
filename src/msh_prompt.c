0cb0c039968ba0bde1317d4252829020ab61a371
etyene <dethomassin.etienne@gmail.com>
2021-02-20 14:29:36 +0100

    Nrme msh_prompt.c

--- src/msh_prompt.c
+++ src/msh_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:59:06 by wendrul           #+#    #+#             */
-/*   Updated: 2021/02/20 13:25:11 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/20 14:28:49 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static char	*gnl(char **old)
 	return (line);
 }
 
-void	clear_list_arr(t_list ***lst)
+void		clear_list_arr(t_list ***lst)
 {
 	t_list	**tmp;
 
@@ -46,62 +46,66 @@ void	clear_list_arr(t_list ***lst)
 	free(*lst);
 }
 
-int		shell(t_builtin builtins)
+t_list		**read_and_syntax(t_command cmd_meta)
 {
-	static int	cmd_num = 0;
-	char		*line;
-	t_command	cmd_meta;
-	t_list		*elements;
-	t_list		**cmds;
-	int			i;
-	int			og_inout[2];
+	t_list	*elements;
+	char	*line;
 
-	(void)&builtins;
 	line = gnl(NULL);
 	while (*line == '\0')
 		line = gnl(&line);
-	cmd_num++;
-	cmd_meta.num = cmd_num;
 	elements = parse_quotes(line, cmd_meta);
 	free(line);
 	if (!elements)
-		return (2);
+		return (NULL);
 	elements = parse_tokens(elements, cmd_meta);
 	if (!syntax_check(elements, cmd_meta))
 	{
 		ft_lstclear(&elements, del_element);
-		return (2);
+		return (NULL);
 	}
-	cmds = get_cmds(elements);
-	i = 0;
-	while (cmds[i])
+	return (get_cmds(elements));
+}
+
+void		reset_io_fds(int og_inout[2])
+{
+	if (g_msh->redir_out_fd != -1)
+		close(g_msh->redir_out_fd);
+	if (g_msh->redir_in_fd != -1)
+		close(g_msh->redir_in_fd);
+	g_msh->redir_out_fd = -1;
+	g_msh->redir_in_fd = -1;
+	dup2(og_inout[0], STDOUT_FILENO);
+	dup2(og_inout[1], STDIN_FILENO);
+	close(og_inout[0]);
+	close(og_inout[1]);
+}
+
+int			shell(t_builtin builtins)
+{
+	static int	cmd_num = 0;
+	t_command	cmd_meta;
+	t_list		**cmds;
+	int			i;
+	int			og_inout[2];
+
+	cmd_num++;
+	cmd_meta.num = cmd_num;
+	if (!(cmds = read_and_syntax(cmd_meta)))
+		return (2);
+	i = -1;
+	while (cmds[++i])
 	{
 		og_inout[0] = dup(STDOUT_FILENO);
 		og_inout[1] = dup(STDIN_FILENO);
-		if (g_msh->verbose)
-		{
-			ft_lstiter(cmds[i], print_el);
-			write(STDOUT_FILENO, "\n", 1);
-		}
 		if (redirections(&cmds[i], cmd_meta) == -1)
 		{
 			clear_list_arr(&cmds);
 			return (2);
 		}
 		dispatch(cmds[i], cmd_meta, builtins);
-		if (g_msh->redir_out_fd != -1)
-			close (g_msh->redir_out_fd);
-		if (g_msh->redir_in_fd != -1)
-			close (g_msh->redir_in_fd);
-		g_msh->redir_out_fd = -1;
-		g_msh->redir_in_fd = -1;
-		dup2(og_inout[0], STDOUT_FILENO);
-		dup2(og_inout[1], STDIN_FILENO);
-		close(og_inout[0]);
-		close(og_inout[1]);
-		i++;
+		reset_io_fds(og_inout);
 	}
 	clear_list_arr(&cmds);
 	return (0);
 }
-
