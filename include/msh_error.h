e758e42cb7705f5729c208ab318d17b9d121dc6f
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2021-02-20 13:44:02 +0100

    normed error.h

--- include/msh_error.h
+++ include/msh_error.h
@@ -3,24 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   msh_error.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agoodwin <agoodwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 17:46:23 by ede-thom          #+#    #+#             */
-/*   Updated: 2021/02/20 12:40:32 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/20 13:43:38 by agoodwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSH_ERROR_H
 # define MSH_ERROR_H
 
-/* Fatal Errors */
+/*
+** Fatal Errors
+*/
 # define MALLOC_FAIL_ERROR "malloc funtion failed to allocate memory, exiting"
 # define FAILED_TO_GET_NEXT_LINE "Failed on get_next_line (malloc?)"
-# define SPLIT_FAIL_ERROR "Failed to split string (malloc or null string input probably)"
+# define SPLIT_FAIL_ERROR "Failed to split string (malloc or null string input)"
 # define FAILED_TO_CREATE_PIPE "Failed to create pipe"
 # define FAILED_FORK "Failed to fork"
 
-/* Normal Errors */
+/*
+** Normal Errors
+*/
 # define NOT_FOUND_ERROR "not found"
 # define SYNTAX_ERROR "Syntax error."
 # define SYNTAX_ERROR_UNEXPECTED "Syntax error: unexpected "
@@ -28,7 +32,9 @@
 # define HOME_IS_NOT_SET "HOME is not set"
 # define UNSPECIFIED_ERROR "Unspecified msh error"
 
-/* Exit statuses */
+/*
+** Exit statuses
+*/
 # define SEGFAULT_STATUS 139
 # define SIGQUIT_STATUS -1
 
@@ -38,10 +44,7 @@
 
 # define NO_HOME_VAR 10
 
- 
-# define EXPECTED_KEY_EQUALS_VALUE_STRING "Expected string of type \
-								'[KEY]=[VALUE]' but no `=` was found"
-
+# define EXPECTED_KEY_EQUALS_VALUE_STRING "Expected format of '[KEY]=[VALUE]'"
 
 void		error_exit(char *str);
 void		simple_error(char *msg, int cmd_no, char *cmd_name);
