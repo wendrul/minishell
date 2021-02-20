5838c9dbffdd45cce940d4b6eda8a6acd41adfe2
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2021-02-20 13:45:15 +0100

    normed utils.h

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agoodwin <agoodwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:18:39 by ede-thom          #+#    #+#             */
-/*   Updated: 2021/02/07 16:28:48 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/02/20 13:45:03 by agoodwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 int		separate(char **half1, char **half2, char sep, char *line);
 int		lst_indexof(int type, t_list *cmd);
 int		separate_at(int index, t_list *cmd, t_list **left, t_list **right);
-void 	lst_append(t_list **head, t_list *tail);
+void	lst_append(t_list **head, t_list *tail);
 
 #endif
