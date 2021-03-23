a15beac06349959ceb19be1b3b9f236abe94852c
Wendrul <dethomassin.etienne@gmail.com>
2021-03-23 21:53:55 +0100

    Modif behaviour on multiple -n for echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: ede-thom <ede-thom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 23:43:58 by ede-thom          #+#    #+#             */
-/*   Updated: 2021/02/02 18:32:15 by ede-thom         ###   ########.fr       */
+/*   Updated: 2021/03/23 21:52:59 by ede-thom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int		msh_echo(int argc, char **argv)
 	endl = 1;
 	if (argc >= 2)
 		endl = !name_cmp(argv[1], "-n");
-	i = endl ? 0 : 1;
+	i = 0;
+	while (name_cmp(argv[i + 1], "-n"))
+		i++;
 	while (++i < argc)
 	{
 		ft_putstr_fd(argv[i], STDOUT_FILENO);
